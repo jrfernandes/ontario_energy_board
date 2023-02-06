874c5a6ba2c3ca6946c5dacf2bd25d523395feb7
N3rdP1um23 <james@matrek.com>
2023-02-05 22:50:39 -0500

    Upgraded deprecated functions

--- custom_components/ontario_energy_board/__init__.py
+++ custom_components/ontario_energy_board/__init__.py
@@ -16,13 +16,13 @@ async def async_setup_entry(hass, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
 
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS)
 
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
