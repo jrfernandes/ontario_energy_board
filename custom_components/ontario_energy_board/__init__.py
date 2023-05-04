8179616bef003ea3bf82e2332f43beee345f2fa5
rtorchia <ralphtorchia1@gmail.com>
2023-05-04 19:09:23 -0400

    Update __init__.py
    change to using async_forward_entry_setups()

--- custom_components/ontario_energy_board/__init__.py
+++ custom_components/ontario_energy_board/__init__.py
@@ -18,8 +18,7 @@ async def async_setup_entry(hass, entry: ConfigEntry):
     await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
-
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
